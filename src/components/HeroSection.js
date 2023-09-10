fdeac17fc8807831fe30883aa90083123c6380d2
Nitin <nitinnegi27.nn@gmail.com>
2023-09-10 21:40:34 +0530

    first commit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,24 +6,16 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
+      {/* <video src='/videos/video-1.mp4' autoPlay loop muted /> */}
+      <h1>MEDICINAL PLANTS</h1>
+      <p>Serach the plant using button below</p>
       <div className='hero-btns'>
         <Button
           className='btns'
           buttonStyle='btn--outline'
           buttonSize='btn--large'
         >
-          GET STARTED
-        </Button>
-        <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
-        >
-          WATCH TRAILER <i className='far fa-play-circle' />
+         UPLOAD IMG
         </Button>
       </div>
     </div>
